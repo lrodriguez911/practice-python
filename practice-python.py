3f572d42d0effde8805809bb718c252e679336d3
lucas-rodriguez <lrodriguez.arevalosc@gmail.com>
2022-08-21 19:53:33 -0300

    practice bolean and if else

--- practice-python.py
+++ practice-python.py
@@ -11,3 +11,35 @@ print(input('Ingrese un numero ') + input('ingrese otro numero '))
 x = 3
 x +=4
 print(x)
+
+"""Boolean y comparadores"""
+if x > 1:
+    print('x es mayor q uno')
+
+"""ejercicio de practica"""
+
+purity = float(input())
+
+if purity >= 91.7:
+    print('Accepted')
+ 
+
+"""declaracion else"""
+
+if x > 10:
+    print('x es mayor q cinco')
+else:
+    print('x no es mayor q cinco')
+
+
+num = 3
+if num == 1:
+    print('One')
+else:
+    if num == 2:
+        print('Two')
+    else:
+        if num == 3:
+            print('Three')
+        else:
+            print('Something else')
