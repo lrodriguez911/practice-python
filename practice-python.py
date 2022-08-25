6e8124675409c7798a0d83e6b8a2ac4c38676838
lucas-rodriguez <lrodriguez.arevalosc@gmail.com>
2022-08-25 11:16:11 -0300

    add exercises complete

--- practice-python.py
+++ practice-python.py
@@ -59,7 +59,12 @@ else:
 
 
 year = 2000
-if year % 4 == 0 and year % 100 == 0 and year % 400 == 0:
-    print("Leap year")
-    else:
-        print("Not a leap year")
+if year % 4 == 0:
+    if year % 100 == 0:
+        if year % 400 == 0:
+            print("Leap year")
+        else:print("Not a leap year")
+    else:print("Leap year")
+else:print("Not a leap year")
+    
+    
