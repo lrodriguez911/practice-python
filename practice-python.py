7543a77f28d4997d6e146f787110dfeef62ae587
lucas-rodriguez <lrodriguez.arevalosc@gmail.com>
2022-09-10 20:57:39 -0300

    add exercesis whit diccionaries

--- practice-python.py
+++ practice-python.py
@@ -465,3 +465,24 @@ print(ages.str) """
     'mileage' : 15000
 }
 print(car[input()]) """
+
+
+nums = {
+    1: "one",
+    2: "two",
+    3: "three",
+}
+print(1 in nums)
+print("three" in nums)
+print(4 not in nums)
+
+
+pairs = {1: "apple",
+  "orange": [2, 3, 4], 
+  True: False, 
+  12: "True",
+}
+
+print(pairs.get("orange"))
+print(pairs.get(7, 42))
+print(pairs.get(12345, "not found"))
