5c7cd5f34eb02648fbcc3fe04efe1ec0fcf97b78
lucas-rodriguez <lrodriguez.arevalosc@gmail.com>
2022-12-03 20:35:59 -0300

    module 2 complete

--- practice-python.py
+++ practice-python.py
@@ -745,3 +745,63 @@ def invoice(num):
     print("INVOICE #" +num)
 
 invoice(input()) """
+
+
+#recursion in python
+
+""" def factorial(x):
+    if x == 1:
+        return 1
+    else: 
+        return x * factorial(x-1)
+
+print(factorial(5)) """
+
+""" def factorial(x):
+    if x == 1:
+        return 1
+    else: 
+        return x * factorial(x-1)
+
+print(factorial(5)) """
+
+""" def fib(x):
+    if x == 0 or x == 1:
+        return 1
+    else:
+        return fib(x-1) + fib(x-2)
+print(fib(4)) """
+
+# test module 2
+
+""" nums = {1,2,3,4,5,6}
+nums = {0,1,2,3} & nums
+nums = filter(lambda x: x > 1, nums)
+print(len(list(nums))) """
+#answer 2
+
+""" def power(x,y):
+    if y == 0:
+        return 1
+    else:
+        return x*power(x, y-1) """
+
+#answer 8
+
+""" a = (lambda x: x * (x+1))(6)
+print(a)
+ """
+#fill in the blanks
+
+""" nums = [1,2,8,3,7]
+res = list(filter(lambda x: x % 2 == 0, nums))
+print(res) """
+
+#fill in the blanks
+
+""" def func(**kwargs):
+    print(kwargs["zero"])
+
+func(a= 0, zero =8) """
+
+#fill in the blanks
