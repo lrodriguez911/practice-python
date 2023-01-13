150ff83b3d933b2d77d774fdd9be57302d44de2d
lucas-rodriguez <lrodriguez.arevalosc@gmail.com>
2023-01-12 23:51:45 -0300

    add new exercises about list

--- datastructures.py
+++ datastructures.py
@@ -23,7 +23,53 @@ print(str3.replace(str1, str2)) """
 #Test Module 1
 #Frecuency of letters
 
-text = "hello"
+""" text = "hello"
 letter = "l"
 
 frecuency = int((text.count(letter)/len(text)) * 100)
+ """
+
+ #operartions whit lists
+""" words = ["spam","egg","spam","sausage"]
+print("spam" in words)
+print("egg" in words)
+print("tomato" in words) """
+
+""" name = input()
+contacts = ["Lucas", "Flor", "Naza"]
+if name in contacts:
+    print("Found")
+else:
+    print("Not found") """
+
+#using a loop to traverse list
+
+""" x = [2,4,6,8]
+
+for n in x:
+    print(n) """
+
+#list exercises the apple of my eyes
+
+#color orders in brown, blue, green and black
+
+data = [[23, 11, 5, 14], 
+[8, 32, 20, 5]]
+
+color = 'blue'
+total = sum(data[0]) + sum(data[1])
+
+result = 0
+if color == 'brown':
+    result = data[0][0] + data[1][0]
+    print(int((result / total)* 100 ))
+elif color == 'blue':
+    result = data[0][1] + data[1][1]
+    print(int((result / total)* 100 ))
+elif color == 'green':
+    result = data[0][2] + data[1][2]
+    print(int((result / total)* 100 ))
+else:
+    result = data[0][3] + data[1][3]
+    print(int((result / total)* 100 ))
+
