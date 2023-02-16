45bd87e7fdf86320d9c939767f88ae1e484e59ae
lucas-rodriguez <lrodriguez.arevalosc@gmail.com>
2023-02-15 23:50:50 -0300

    straing whit queue

--- datastructures.py
+++ datastructures.py
@@ -275,3 +275,23 @@ x.push('www.sololearn.com/courses/python/')
 
 while not x.is_empty():
     print(x.pop())
+
+
+#Queue
+
+class Queue:
+    def __init__(self):
+      self.items = []
+
+    def is_empty(self):
+      return self.items == []
+    
+    def enqueue(self, item):
+       self.items.insert(0, item)
+
+    def dequeue(self):
+       return self.items.pop()
+    
+    def print_queue(self):
+       print(self.items)
+   
