3b1a68f28ce37ef972005b7989d52c47123fff10
lucas-rodriguez <lrodriguez.arevalosc@gmail.com>
2023-03-22 23:44:12 -0300

    add new exercises to solve

--- datastructures.py
+++ datastructures.py
@@ -295,3 +295,88 @@ class Queue:
     def print_queue(self):
        print(self.items)
    
+
+#Linked List
+
+class Node:
+    def __init__(self, data, next):
+       self.data = data
+       self.next = next
+
+class LinkedList:
+    def __init__(self):
+       self.head = None
+    
+    def add_at_front(self, data):
+        self.head = Node(data, self.head)
+    
+    def add_at_end(self, data):
+        if not self.head:
+            self.head = Node(data, None)
+            return
+        curr = self.head
+        while curr.next:
+            curr = curr.next
+        curr.next = Node(data, None)
+    
+    def get_last_node(self):
+        n = self.head
+        while (n.next != None):
+            n = n.next
+        return n.data
+
+    def is_empty(self):
+        return self.head == None
+    
+    def print_list(self):
+        n = self.head
+        while n != None:
+            print(n.data, end=" => ")
+            n = n.next
+        print()
+
+s = LinkedList()
+s.add_at_front(5)
+s.add_at_end(8)
+s.add_at_front(9)
+
+s.print_list()
+print(s.get_last_node())
+
+            
+# Exercises Name that tune
+
+class Track:
+    def __init__(self, title, next):
+        self.title = title
+        self.next = next
+
+class Player:
+    def __init__(self):
+        self.head = None
+
+    def add(self, title):
+        if not self.head:
+            self.head = Track(title, None)
+            return
+        curr = self.head
+        while curr.next:
+            curr = curr.next
+        curr.next = Track(title, None)
+
+
+p = Player()
+""" while True:
+    x = input()
+    if x == 'end':
+        break
+    p.add(x) """
+
+p.add("bib")
+p.add("myg")
+p.add("lc")
+p.add("believe")
+#your code goes here
+#your code goes here
+""" while n != None: """
+print(p.head.title)
